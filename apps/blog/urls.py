be31e586a0a913c72d52dd7269449720ddfeef08
Mayko2003 <maykox34f@gmail.com>
2022-01-18 03:34:26 -0300

    Agregado templates II

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import *
+
+
 urlpatterns = [
     path('', home, name = 'home'),
     path('about', about, name = 'about'),
     path('contact', contact, name = 'contact'),
+    path('ver-post/<slug:slug>', ver_post, name = 'ver_post')
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
-
+from .models import *
+from django.shortcuts import get_object_or_404
+from django.db.models import Q
+from django.core.paginator import Paginator
 # Create your views here.
 
 def home(request):
-    return render(request, 'index.html')
+    posts = Post.objects.filter(estado = True)
+    query = request.GET.get("buscar_post")
+    if query:
+        posts = Post.objects.filter(
+            Q(titulo__icontains = query) |
+            Q(descripcion__icontains = query)
+        ).distinct()
+    paginator = Paginator(posts, 1)
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
+    return render(request, 'index.html', {'posts' : posts})
 
 
 def about(request):
@@ -11,3 +24,7 @@ def about(request):
 
 def contact(request):
     return render(request, 'contact.html')
+
+def ver_post(request, slug):
+    post = get_object_or_404(Post, slug = slug)
+    return render(request, 'post.html', {'post' : post})

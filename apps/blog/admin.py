439097a70efc5bdefa583239e4cf0d551c3b52e1
Mayko2003 <maykox34f@gmail.com>
2022-01-17 21:19:31 -0300

    creacion de modelos II y vista admin I

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,7 +1,29 @@
 from django.contrib import admin
 from .models import *
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin, ImportExportActionModelAdmin
+# Register custom admin models here.
+
+class CategoriaResource(resources.ModelResource):
+    class Meta:
+        model = Categoria
+
+class UsuarioResource(resources.ModelResource):
+    class Meta:
+        model = Usuario
+
+class CategoriaAdmin(ImportExportActionModelAdmin, ImportExportModelAdmin, admin.ModelAdmin):
+    search_fields = ['nombre']
+    list_display = ['nombre', 'estado', 'fecha_creacion']
+    resource_class = CategoriaResource
+
+class UsuarioAdmin(ImportExportActionModelAdmin, ImportExportModelAdmin, admin.ModelAdmin):
+    search_fields = ['nombre_usuario', 'nombres', 'apellidos', 'email']
+    list_display = ['nombre_usuario', 'nombres', 'apellidos', 'email', 'estado', 'fecha_creacion']
+    resource_class = UsuarioResource
+
 
 # Register your models here.
-admin.site.register(Categoria)
-admin.site.register(Usuario)
+admin.site.register(Categoria, CategoriaAdmin)
+admin.site.register(Usuario, UsuarioAdmin)
 
--- apps/blog/models.py
+++ apps/blog/models.py
@@ -5,37 +5,38 @@ from django.db import models
 
 class Categoria(models.Model):
     id = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=100, unique=True, null=False, blank=False)
+    nombre = models.CharField(
+        max_length=100, unique=True, null=False, blank=False)
     estado = models.BooleanField(default=True)
-    fecha_creacion = models.DateTimeField('Fecha de creacion', auto_now=False ,auto_now_add=True)
-    
+    fecha_creacion = models.DateTimeField(
+        'Fecha de creacion', auto_now=False, auto_now_add=True)
+
     class Meta:
         verbose_name = 'Categoria'
         verbose_name_plural = 'Categorias'
         db_table = 'categorias'
-        
+
     def __str__(self):
         return self.nombre
-    
+
 
 class Usuario(models.Model):
     id = models.AutoField(primary_key=True)
-    nombre_usuario = models.CharField('Nombre de usuario',max_length=100, unique=True, null=False, blank=False)
+    nombre_usuario = models.CharField(
+        'Nombre de usuario', max_length=100, unique=True, null=False, blank=False)
     nombres = models.CharField(max_length=100, null=False, blank=False)
     apellidos = models.CharField(max_length=100, null=False, blank=False)
-    email = models.EmailField(max_length=100, unique=True, null=False, blank=False)
+    email = models.EmailField(
+        max_length=100, unique=True, null=False, blank=False)
     estado = models.BooleanField(default=True)
-    fecha_creacion = models.DateTimeField('Fecha de creacion', auto_now=False ,auto_now_add=True)
-    
+    fecha_creacion = models.DateTimeField(
+        'Fecha de creacion', auto_now=False, auto_now_add=True)
+
     class Meta:
         verbose_name = 'Usuario'
         verbose_name_plural = 'Usuarios'
         db_table = 'usuarios'
-        
+
     def __str__(self):
-        return "{nombres} {apellidos}".format(nombres=self.nombres, apellidos=self.apellidos)
-    
-
-    
-
-    
+        return "{nombre_usuario}, {nombres} {apellidos}".format(
+            nombre_usuario=self.nombre_usuario, nombres=self.nombres, apellidos=self.apellidos)
